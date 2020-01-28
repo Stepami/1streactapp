c4ef720a1a53980d83ee98ca71d0947c957b6d78
Stepami <Stepami@mail.ru>
2020-01-28 13:45:56 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader'] //загрузчики выполняются справа налево
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 9000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html" // исходный html
+        })
+    ]
+};
