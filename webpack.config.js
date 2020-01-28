cd91fb14b4000913214da09053b8c4802ee91957
Stepami <Stepami@mail.ru>
2020-01-29 00:42:50 +0300

    подготовка проекта к миграции на TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx"]
+    },
     entry: path.resolve(__dirname, 'src/index'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -9,6 +15,18 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.ts(x?)$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: ['ts-loader']
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
             {
                 test: /\.(js|jsx)$/,
                 include: path.resolve(__dirname, 'src'),
@@ -30,5 +48,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "src/index.html" // исходный html
         })
-    ]
+    ],
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
 };
